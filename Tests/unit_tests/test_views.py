4d6695527377fcde17066f01749b22a2a2a666b5
Felix-Ching <D0978214@o365.fcu.edu.tw>
2023-01-02 00:26:11 +0800

    Debug test in test_views.py(1)

--- Tests/unit_tests/test_views.py
+++ Tests/unit_tests/test_views.py
@@ -40,7 +40,7 @@ class ViewTest(TestCase):
 
     def test_unit_height0(self):
         """Tests the home page."""
-        response = self.client.post('/', {'height':0,'weight':"60"})
+        response = self.client.post('/', {'height':-0.1,'weight':"60"})
         self.assertContains(response, '請輸入合理範圍的身高!', 1, 200, html=True)
 
     def test_unit_height25(self):
